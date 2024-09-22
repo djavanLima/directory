6caf2d089cc4a7786f3d8f6f62a41c2858b03725
Djavan Lima <djavanlyonel@gmail.com>
2024-09-22 10:17:20 -0400

    adicao de fileexception em error handler

--- src/main/java/br/com/github/djavanlima/directory/controller/apierror/ErrorHandler.java
+++ src/main/java/br/com/github/djavanlima/directory/controller/apierror/ErrorHandler.java
@@ -9,11 +9,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import br.com.github.djavanlima.directory.service.exception.DirectoryNotFoundException;
+import br.com.github.djavanlima.directory.service.exception.FileNotFoundException;
 
 @RestControllerAdvice
 public class ErrorHandler {
     
-    @ExceptionHandler({DirectoryNotFoundException.class})
+    @ExceptionHandler({
+		DirectoryNotFoundException.class,
+		FileNotFoundException.class
+	})
     public ResponseEntity<List<String>> handleNotFoundException(RuntimeException ex) {
 		List<String> mensagem = new ArrayList<>();
 		mensagem.add(ex.getMessage());
